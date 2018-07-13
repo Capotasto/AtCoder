747a74073148ebbd2c9240582007dc36df4c2990
Capotasto <rocker519804@gmail.com>
2018-07-12 17:32:07 -0700

    Fixed Practice1

--- src/com/funkyhacker/Practice1.java
+++ src/com/funkyhacker/Practice1.java
@@ -1,15 +1,15 @@
 package com.funkyhacker;
 
+import java.util.Scanner;
+
 public class Practice1 {
     public static void main(String[] args) {
-        System.out.println(args.length);
-        int sum = 0;
-        for (int i = 0; i < args.length; i++) {
-            if (i < 3) {
-                sum += Integer.parseInt(args[i]);
-            } else {
-                System.out.format("%s %s", String.valueOf(sum), args[i]);
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        String s = scanner.next();
+
+        System.out.format("%d %s\n", a+b+c, s);
     }
 }
