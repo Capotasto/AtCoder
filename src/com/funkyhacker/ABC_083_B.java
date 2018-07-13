340b8eab9d196018d6340f886010f61b437114c1
Capotasto <rocker519804@gmail.com>
2018-07-13 13:18:08 -0700

    Refactoed ABC_083_B

--- src/com/funkyhacker/ABC_083_B.java
+++ src/com/funkyhacker/ABC_083_B.java
@@ -13,7 +13,7 @@ public class ABC_083_B {
         int answer = 0;
 
         for (int i = 1 ; i <= n ; i++) {
-            int digitSum = getDigitSum(String.valueOf(i));
+            int digitSum = getDigitSum(i);
             if (a <= digitSum && digitSum <= b) {
                 answer += i;
             }
@@ -21,10 +21,21 @@ public class ABC_083_B {
         System.out.println(answer);
     }
 
+    //
     private static int getDigitSum(String input) {
         int result = 0;
         for (int i = 0; i < input.length(); i++) {
             result += Character.getNumericValue(input.charAt(i));
+        }
+        return result;
+    }
+
+    //There is another way to get Digit Sum
+    private static int getDigitSum(int value) {
+        int result = 0;
+        while (value > 0) {
+            result += value % 10;
+            value /= 10;
         }
         return result;
     }
